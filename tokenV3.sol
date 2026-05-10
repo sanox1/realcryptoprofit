403b4fe486c7485f52ee5628abd075f2d3b92b70
kester <invailor@mail.bg>
2026-05-10 10:30:29 +0300

    Implement ERC20 token with burn and ownership functions

--- tokenV3.sol
+++ tokenV3.sol
@@ -1,1 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract RCPT is ERC20Permit, Ownable {
+    constructor(uint256 initialSupply) 
+        ERC20Permit("RealCryptoProfit") 
+        ERC20("RealCryptoProfit", "RCP") 
+    {
+        if (initialSupply > 0) {
+            _mint(msg.sender, initialSupply * 10 ** decimals());
+        }
+    }
+    
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+    
+    function renounceOwnershipOfContract() external onlyOwner {
+        renounceOwnership();
+    }
+}
